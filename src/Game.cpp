960285a742c4698a6f50acd7ccb9d01ffad6fa57
Hopson97 <mhopson@hotmail.co.uk>
2017-08-19 10:33:20 +0100

    Implemnt fixed update

--- src/Game.cpp
+++ src/Game.cpp
@@ -3,27 +3,53 @@
 #include "state/PlayingState.h"
 
 Game::Game()
+:   m_window    ({1280, 720}, "Zombie Game")
 {
     pushState<PlayingState>(*this);
 }
 
 void Game::runGame()
 {
+    unsigned tickCount = 0;
+    constexpr unsigned  UPDATES_PER_FRAME = 30;
+    const sf::Time      MS_PER_UPDATE       = sf::seconds(1.0f / float(UPDATES_PER_FRAME));
+
     sf::Clock timer;
+    sf::Time  lastTime = timer.getElapsedTime();
+    sf::Time  lag      = sf::Time::Zero;
+
     while (m_window.isOpen() && !m_states.empty())
     {
         auto& state = getCurrentState();
 
+        //Time handling for the fixed update and
+        //getting delta-time
+        auto time      = timer.getElapsedTime();
+        auto elapsed   = time - lastTime;
+
+        lastTime = time;
+        lag     += elapsed;
+
+        //Real time updates
         state.handleInput();
-        state.update(timer.restart());
+        state.update(elapsed);
 
-        //fixed update here
+        //Fixed update
+        while (lag > MS_PER_UPDATE)
+        {
+            tickCount++;
+            lag -= MS_PER_UPDATE;
+            state.fixedUpdate(elapsed);
+        }
 
+        //Render
         m_window.clear();
         state.render(m_window);
         m_window.display();
+
+        //done last so that window closing/ state pointer dangling does not cause crash
+        handleEvents();
         tryPop();
-        handleEvents(); //done last so that window closing does not cause crash
     }
 }
 
