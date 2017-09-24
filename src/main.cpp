21b3c8cb6260ac783fe0a52b2dbed374c35a6e27
Vaionescu <Vaionescu@users.noreply.github.com>
2017-09-24 12:48:57 +0300

    Edited main.cpp and CMakeLists.txt, therefore making the project build completely (#24)

--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,24 @@
-<<<<<<< HEAD
-#include <SFML/Graphics.hpp>
+//#include <SFML/Graphics.hpp>
 
-int main()
-{
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+//int main()
+//{
+//    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+//    sf::CircleShape shape(100.f);
+//    shape.setFillColor(sf::Color::Green);
 
-    while (window.isOpen())
-    {
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
+//    while (window.isOpen())
+//    {
+ //       sf::Event event;
+   //     while (window.pollEvent(event))
+     //   {
+       //     if (event.type == sf::Event::Closed)
+         //       window.close();
+       // }
 
-        window.clear();
-        window.draw(shape);
-        window.display();
-    }
-=======
+        //window.clear();
+//        window.draw(shape);
+  //      window.display();
+    //}
 #include "Game.h"
 
 #include "gameobject/GameObject.h"
@@ -32,7 +30,5 @@ int main()
 {
     Game game;
     game.runGame();
->>>>>>> 9990422359331f2330abfad091c9c755b5d21515
-
     return 0;
 }
